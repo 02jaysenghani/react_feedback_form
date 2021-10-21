126fd3dce70c4f42467304c5c5ec28c3a6c2fa00
Jay Senghani <02jaysenghani@gmail.com>
2021-10-21 13:44:28 +0530

    remove unwated files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from "./component/App"
 import { Provider } from "react-redux";
 import store from "./__redux/store"
